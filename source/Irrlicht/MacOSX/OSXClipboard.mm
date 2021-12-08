ed7634376eed378349b554cf3bdd9b2e1869d0e7
mercury233 <me@mercury233.me>
2021-12-08 19:56:44 +0800

    fix mac copy&paste

--- source/Irrlicht/MacOSX/OSXClipboard.mm
+++ source/Irrlicht/MacOSX/OSXClipboard.mm
@@ -3,34 +3,35 @@
 // This file is part of the "Irrlicht Engine".
 // For conditions of distribution and use, see copyright notice in Irrlicht.h
 
+#include "irrString.h"
 #include "OSXClipboard.h"
 #import <Cocoa/Cocoa.h>
 
-void OSXCopyToClipboard(const char *text)
+void OSXCopyToClipboard(const wchar_t *text)
 {
 	NSString *str;
 	NSPasteboard *board;
 
-	if ((text != NULL) && (strlen(text) > 0))
+	if ((text != NULL) && (wcslen(text) > 0))
 	{
-		str = [NSString stringWithCString:text encoding:NSWindowsCP1252StringEncoding];
+		str = [[NSString alloc] initWithBytes:text length:wcslen(text)*sizeof(*text) encoding:NSUTF32LittleEndianStringEncoding];
 		board = [NSPasteboard generalPasteboard];
 		[board declareTypes:[NSArray arrayWithObject:NSStringPboardType] owner:NSApp];
 		[board setString:str forType:NSStringPboardType];
 	}
 }
 
-char* OSXCopyFromClipboard()
+wchar_t* OSXCopyFromClipboard()
 {
 	NSString* str;
 	NSPasteboard* board;
-	char* result;
+	wchar_t* result;
 
 	result = NULL;
 	board = [NSPasteboard generalPasteboard];
 	str = [board stringForType:NSStringPboardType];
 	if (str != nil)
-		result = (char*)[str cStringUsingEncoding:NSWindowsCP1252StringEncoding];
+		result = (wchar_t*)[str cStringUsingEncoding:NSUTF32LittleEndianStringEncoding];
 	return (result);
 }
 
