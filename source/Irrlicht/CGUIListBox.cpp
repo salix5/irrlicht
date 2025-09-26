66fe6cb7a9b9c3c06607114a6e44e8e1a44eef91
salix5 <salix5566@gmail.com>
2025-09-26 23:33:56 +0800

    fix EGET_LISTBOX_CHANGED event

--- source/Irrlicht/CGUIListBox.cpp
+++ source/Irrlicht/CGUIListBox.cpp
@@ -326,6 +326,7 @@ bool CGUIListBox::OnEvent(const SEvent& event)
 
 				// find the selected item, starting at the current selection
 				s32 start = Selected;
+				s32 oldSelected = Selected;
 				// dont change selection if the key buffer matches the current item
 				if (Selected > -1 && KeyBuffer.size() > 1)
 				{
@@ -342,7 +343,7 @@ bool CGUIListBox::OnEvent(const SEvent& event)
 						if (KeyBuffer.equals_ignore_case(Items[current].text.subString(0,KeyBuffer.size())))
 						{
 							setSelected(current);
-							if (Parent && Selected != current && !Selecting && !MoveOverSelect)
+							if (Parent && oldSelected != current && !Selecting && !MoveOverSelect)
 							{
 								SEvent e;
 								e.EventType = EET_GUI_EVENT;
@@ -362,7 +363,7 @@ bool CGUIListBox::OnEvent(const SEvent& event)
 						if (KeyBuffer.equals_ignore_case(Items[current].text.subString(0,KeyBuffer.size())))
 						{
 							setSelected(current);
-							if (Parent && Selected != current && !Selecting && !MoveOverSelect)
+							if (Parent && oldSelected != current && !Selecting && !MoveOverSelect)
 							{
 								Selected = current;
 								SEvent e;
